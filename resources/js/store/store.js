be8c11802af26d465bbedab7d18ec8ebe42ab83e
tranduyphe <phetranduy@gmail.com>
2023-04-15 16:41:14 +0700

    Update function create task use role admin

--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -4,14 +4,12 @@ import Permissions from './modules/permissions';
 import Auth from './modules/auth';
 import Project from './modules/project';
 import Task from './modules/task';
-import Label from './modules/label';
 export const store = createStore({
     modules: {
         Roles,
         Permissions,
         Auth,
         Project,
-        Task, 
-        Label
+        Task,
     },
 });
