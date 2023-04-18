adbb22d2d6a0dc2beabd24b74983501b8bb7310f
tranduyphe <phetranduy@gmail.com>
2023-04-18 16:43:04 +0700

    Updated function edit change profile user

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\User;
 use Illuminate\Support\Facades\Auth; 
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\DepartmentUser;
+use App\Models\Role;
 
 class AuthController extends Controller
 {
@@ -223,7 +225,37 @@ class AuthController extends Controller
      */
     public function index()
     {
-        //
+        $user     = Auth::user();
+        $user_id  = $user->id;
+        $isAdmin  = $user->hasAnyRole('administrator');
+        $users    = [];
+        if($isAdmin){
+            $users = User::with(['roles' => function ($query) {
+                $query->where('status', 1);
+            }])
+            ->get()->toArray();
+        }else{
+            $deparmentUser = DepartmentUser::where('user_id', $user_id)->get()->first();
+            if($deparmentUser){
+                $listUser = DepartmentUser::where('department_id', $deparmentUser->department_id)
+                ->pluck('user_id')
+                ->toArray();
+                $users = User::with(['roles' => function ($query) {
+                    $query->where('status', 1);
+                }])
+                ->whereIn('id', $listUser)
+                ->get()->toArray();
+            }
+        }
+        $roles = Role::where('status', 1)->get()->toArray();
+
+        $responseData = [
+            'status' => 200,
+            'success'=>true,
+            'message' => 'success',
+            'data' => ['users'=>$users,'roles'=>$roles]
+        ];
+        return response()->json($responseData);
     }
 
     /**
@@ -231,9 +263,45 @@ class AuthController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        $role = Role::where('id', $request->input('role'))->get()->first();
+        $oldUser = User::where('email', $request->input('email'))->get()->first();
+        if($oldUser){
+            $responseData = [    'status' => 200,'success'=>true,    'message' => 'This account already exists'];
+            return response()->json($responseData);
+        }
+        if ($role){            
+            $user = User::create([
+                'name' => $request->input('name'),
+                'email' => $request->input('email'),
+                'password' => Hash::make(""),
+                'first_login'=> 1
+            ]);
+            $user->assignRole($role->name);
+            $user->load([
+                'roles' => function ($query) {
+                    $query->where('status', 1);
+                },
+                'roles.permissions' => function ($query) {
+                    $query->where('status', 1);
+                }
+            ]);
+            $responseData = [
+                'status' => 200,
+                'success'=>true,
+                'message' => 'The user successfully created',
+                'data'=>['user_created'=>$user]
+            ];
+            return response()->json($responseData);
+        }else{
+            $responseData = [
+                'status' => 200,
+                'success'=>false,
+                'message' => 'Role not found'
+            ];
+            return response()->json($responseData);
+        }
     }
 
     /**
@@ -276,11 +344,110 @@ class AuthController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request)
     {
-        //
+        $user = $request->user();
+        $userUpdate = User::find($user->id);
+        $userUpdate->last_name = $request->input('last_name');
+        $userUpdate->first_name = $request->input('first_name');
+        $userUpdate->phone = $request->input('phone');
+        $userUpdate->address = $request->input('address');
+        $userUpdate->save();
+        $responseData = [
+            'status' => 200,
+            'success'=>true,
+            'message' => 'The user successfully updated'];
+        return response()->json($responseData);
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function changeRoleUser($id, Request $request)
+    {
+        $user = User::find($id);
+        if($user){
+            $user->load([
+                'roles' => function ($query) {
+                    $query->where('status', 1);
+                }
+            ]);
+            $roles = Role::where('status', 1)->get();
+            foreach ($roles as $role) {
+                if ($request->has('role_'. $role->id) && $request->input('role_'. $role->id) == true){
+                    $user->assignRole($role->name);
+                }else{
+                    $user->removeRole($role->name);
+                }
+            }
+            $user->load([
+                'roles' => function ($query) {
+                    $query->where('status', 1);
+                },
+            ]);
+            $responseData = [
+                'status' => 200,
+                'success'=>true,
+                'message' => 'The user successfully updated',
+                'data'=>['user_update'=>$user]
+            ];
+            return response()->json($responseData);
+        }else{
+            $responseData = [
+                'status' => 200,
+                'success'=>true,
+                'message' => 'The user not found'
+            ];
+        return response()->json($responseData);
+        }
     }
 
+    // update user
+    public function changePasswordUser($id, Request $request)
+    {
+        $user = User::find($id);
+        $password = $request->input('password');
+        $repassword = $request->input('repassword');
+        if($password !== $repassword or !$password){
+            $responseData = ['status' => 200,'success'=>true,    'message' => 'The user false to updated'];
+            return response()->json($responseData);
+        }
+        $user->password = Hash::make($password);
+        $user->save();
+        $responseData = [
+            'status' => 200,'success'=>true,
+            'message' =>
+            'The user successfully updated'];
+        return response()->json($responseData);
+    }
+    /**
+     * Xử lý yêu cầu đăng nhập.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function changePassword(Request $request)
+    {
+        $user = $request->user();
+        $userUpdate = User::find($user->id);
+        // return response()->json($user->password);
+        if (!Hash::check($request->input('password'), $user->password)) {
+            $responseData = [   'status' => 200,'success'=>true,     'message' => 'Password false'];
+            return response()->json($responseData);
+            
+        }
+        $passwordNew = $request->input('password_new');
+        $repasswordNew = $request->input('repassword_new');
+        if($passwordNew !== $repasswordNew or !$passwordNew){
+            $responseData = [   'status' => 200,'success'=>true,     'message' => 'The user false to updated'];
+            return response()->json($responseData);
+        }
+        $user->password = Hash::make($passwordNew);
+        $user->save();
+        $responseData = [   'status' => 200,'success'=>true,     'message' => 'The user successfully updated'];
+        return response()->json($responseData);
+    }
     /**
      * Remove the specified resource from storage.
      *
@@ -291,4 +458,7 @@ class AuthController extends Controller
     {
         //
     }
-}
+}    
+
+    
+    
