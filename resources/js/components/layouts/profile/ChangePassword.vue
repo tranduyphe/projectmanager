756e0f6ec9779ac492b0992316489798ca19c1bf
unknown <khanhloi3011@gmail.com>
2023-04-21 11:24:25 +0700

    capnhat

--- resources/js/components/layouts/profile/ChangePassword.vue
+++ resources/js/components/layouts/profile/ChangePassword.vue
@@ -15,7 +15,7 @@
                 <i class="fas fa-user text-secondary mx-3"></i>
                 <router-link :to="{ name: 'Profile User' }" class="text-secondary">Profile</router-link>
               </div>
-              <div class="form-group px-5 pb-3">
+              <div class="form-group px-5 pb-3 mt-3">
                 <i class="fas fa-lock text-secondary mx-3"></i>
                 <router-link :to="{ name: 'Change Password' }" class="text-secondary">Change Password</router-link>
               </div>
@@ -28,26 +28,26 @@
         <div class="card show border border-0 profile-card-body">
           <div class="card-body">
             <h4 class="card-title text-center fs-4">Change Password</h4>
-            <div class="col-md-12">
+            <div class="col-md-12 mt-3">
               <form @submit.prevent="updatePasswordUser">
                 <div class="row">
                   <div class="col-md-12">
                     <div class="form-group">
                       <label class="label-item">Old Password</label>
-                      <input type="password" class="form-control" required v-model="userForm.password" />
+                      <input type="password" class="form-control mt-1" required v-model="userForm.password" />
                     </div>
-                    <div class="form-group">
+                    <div class="form-group mt-2">
                       <label class="label-item">New Password</label>
-                      <input type="password" class="form-control" required v-model="userForm.password_new" />
+                      <input type="password" class="form-control mt-1" required v-model="userForm.password_new" />
                     </div>
-                    <div class="form-group">
+                    <div class="form-group mt-2">
                       <label class="label-item">Re-enter password</label>
-                      <input type="password" class="form-control" required v-model="userForm.repassword_new" />
+                      <input type="password" class="form-control mt-1" required v-model="userForm.repassword_new" />
                     </div>
                   </div>
                 </div>
-                <div class="d-flex justify-content-center">
-                  <button type="submit" class="btn btn-all-add-edit py-2 px-4">
+                <div class="d-flex">
+                  <button type="submit" class="btn btn-all-add-edit py-2 px-4 mt-4 btn-primary">
                     Save
                   </button>
                 </div>
